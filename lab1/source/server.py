99b4983629c9f2dbb77d6f16947e02b159372030
hmsalamaga <hmsalamaga@gmail.com>
2022-11-05 14:36:52 +0100

    Add line with current time

--- lab1/source/server.py
+++ lab1/source/server.py
@@ -2,21 +2,27 @@
 import http.server
 import socketserver
 import os
+import time
 
 #print('source code for "http.server":', http.server.__file__)
+
+os.environ['TZ'] = 'Europe/Warsaw'
+time.tzset()
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
 
         print(self.path)
-        
+        current_time = time.strftime("%H:%M:%S")
+        print(current_time)
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()            
             self.wfile.write(b"Hello World!\n")
+            self.wfile.write(str.encode(f'{current_time}\n'))
         else:
             super().do_GET()
     
