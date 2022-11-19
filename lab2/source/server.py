b7849ef305b3dd62dbf915325a85a10ad08cfbd3
hmsalamaga <hmsalamaga@gmail.com>
2022-11-19 11:19:52 +0100

    Implement rev command

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -10,13 +11,23 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
+        parsed_path = urlparse(self.path)
         
-        if self.path == '/':
+        if parsed_path.path == '/':
+            query_params = parse_qs(parsed_path.query)
+
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.end_headers()
+            self.wfile.write(str.encode(f'{query_params}\n'))
+
+            if query_params['cmd'] == ['rev']:
+                reversed_strgs = [strg[::-1] for strg in query_params['str']]
+                self.wfile.write(str.encode(f'{reversed_strgs}\n'))
+            else:
+                self.wfile.write(b"Hello World!\n")
+            
         else:
             super().do_GET()
     
