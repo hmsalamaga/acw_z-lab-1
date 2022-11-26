eb278504b1b5526f1f8c504e2eb17a054c68b387
hmsalamaga <hmsalamaga@gmail.com>
2022-11-26 10:34:00 +0100

    Change html to json response

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 from urllib.parse import urlparse, parse_qs
+import json
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -17,11 +18,11 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.send_header('Content-type', 'application/json; charset=UTF-8')
             self.end_headers()
             if query_params.get('str', None):
                 strg = query_params.get('str', None)[0]
-                self.wfile.write(str.encode(f'{strg}\n'))
+                self.wfile.write(str.encode(json.dumps({'received': strg})))
         else:
             super().do_GET()
     
