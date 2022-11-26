be0ed22e1cf5644e2ee31c6a7599f35330b5656c
hmsalamaga <hmsalamaga@gmail.com>
2022-11-26 10:28:45 +0100

    Handle str parameter

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import http.server
 import socketserver
-import os
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -10,13 +10,18 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
+        parsed_path = urlparse(self.path)
         
-        if self.path == '/':
+        if parsed_path.path == '/':
+            query_params = parse_qs(parsed_path.query)
+
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.end_headers()
+            if query_params.get('str', None):
+                strg = query_params.get('str', None)[0]
+                self.wfile.write(str.encode(f'{strg}\n'))
         else:
             super().do_GET()
     
